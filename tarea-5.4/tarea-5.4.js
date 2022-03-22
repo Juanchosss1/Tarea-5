8676787b97208541b99b7c673d8104a060c257bc
Juanchosss1 <juancruzperez933@gmail.com>
2022-03-22 19:43:56 -0300

    Correccion y cambio
    SE intenta corregir y completar funciones de mayor, menor, promedio y repetido

--- tarea-5.4/tarea-5.4.js
+++ tarea-5.4/tarea-5.4.js
@@ -1,10 +1,50 @@
 const $botonCalcular = document.querySelector("button");
 
+function obtenerNumeroMenor(arrayDeNumeros){
+    let numeroMenor = arrayDeNumeros[0];
+    for (let i = 0; i <= arrayDeNumeros.lenght; i++){
+        if(arrayDeNumeros[i] < numeroMenor){
+            numeroMenor = arrayDeNumeros[i];
+        }
+    }
+    return numeroMenor;
+}
+
+function obtenerNumeroMayor(arrayDeNumeros){
+    let numeroMayor = arrayDeNumeros[0];
+    for (i = 0; i <= arrayDeNumeros.lenght; i++){
+        if( arrayDeNumeros[i] > numeroMayor){
+            numeroMayor = arrayDeNumeros[i];
+        }
+    }
+    return numeroMayor
+}
+
+function calcularPromedio(arrayDeNumeros){
+    let promedio = arrayDeNumeros[0];
+    for (i = 0; i <= arrayDeNumeros.lenght; i++){
+        promedio += arrayDeNumeros[i];
+    }
+    return promedio/arrayDeNumeros.lenght;
+}
+
+function numeroMasRepetido(arrayDeNumeros){
+
+}
+const arrayDeNumeros = [];
+
 $botonCalcular.onclick = function(){
-    const $nodeListValores = [...document.querySelectorAll("li")];
-    console.log($nodeListValores);
-    //const arrayValores = Array.from($nodeListValores);
-    //const min = Math.min(...arrayValores);
-    //console.log(arrayValores);
+    const $nodeListValores = document.querySelectorAll("numero");
+    
+    for (let i = 0; i <= $nodeListValores.lenght; i++) {
+        arrayDeNumeros.push(Number($nodeListValores[i].textContent))     
+    }
+    
     
 }
+
+
+document.querySelector('#numero-mas-pequeño').textContent = obtenerNumeroMenor(arrayDeNumeros);
+document.querySelector('#numero-mas-grande').textContent = obtenerNumeroMayor(arrayDeNumeros);
+document.querySelector('#numeros-promedio').textContent = Math.floor(calcularPromedio(arrayDeNumeros));
+document.querySelector('#numero-mas-repetido').textContent = obtenerNumeroMasRepetido(arrayDeNumeros);
