aa54749c5b6479e14b99f17a1113041f81d3ca45
Juanchosss1 <juancruzperez933@gmail.com>
2022-04-18 20:06:16 -0300

    Correccion de error en numero-mas-grande
    Se corrigue error en elemento em "numero-mas-grande", el mismo entraba en conflicto con el promedio (Dios sabe porque)

--- tarea-5.4/tarea-5.4.js
+++ tarea-5.4/tarea-5.4.js
@@ -1,50 +1,79 @@
-const $botonCalcular = document.querySelector("button");
+const $numeros = document.querySelectorAll(".numeros");
 
-function obtenerNumeroMenor(arrayDeNumeros){
-    let numeroMenor = arrayDeNumeros[0];
-    for (let i = 0; i <= arrayDeNumeros.lenght; i++){
-        if(arrayDeNumeros[i] < numeroMenor){
-            numeroMenor = arrayDeNumeros[i];
+
+
+
+
+function obtenerNumeroMenor(){
+    let numeroMenor = Number($numeros[0].innerText);
+    for (let i = 0; i < $numeros.length; i++){
+        if(Number($numeros[i].innerText) < numeroMenor){
+            numeroMenor = Number($numeros[i].innerText);
         }
     }
     return numeroMenor;
 }
 
-function obtenerNumeroMayor(arrayDeNumeros){
-    let numeroMayor = arrayDeNumeros[0];
-    for (i = 0; i <= arrayDeNumeros.lenght; i++){
-        if( arrayDeNumeros[i] > numeroMayor){
-            numeroMayor = arrayDeNumeros[i];
+function obtenerNumeroMayor(){
+    let numeroMayor = Number($numeros[0].innerText);
+    for (i = 0; i < $numeros.length; i++){
+        if(Number($numeros[i].innerText) > numeroMayor){
+            numeroMayor = Number($numeros[i].innerText);
         }
     }
     return numeroMayor
 }
 
-function calcularPromedio(arrayDeNumeros){
-    let promedio = arrayDeNumeros[0];
-    for (i = 0; i <= arrayDeNumeros.lenght; i++){
-        promedio += arrayDeNumeros[i];
-    }
-    return promedio/arrayDeNumeros.lenght;
-}
-
-function numeroMasRepetido(arrayDeNumeros){
-
-}
-const arrayDeNumeros = [];
-
-$botonCalcular.onclick = function(){
-    const $nodeListValores = document.querySelectorAll("numero");
-    
-    for (let i = 0; i <= $nodeListValores.lenght; i++) {
-        arrayDeNumeros.push(Number($nodeListValores[i].textContent))     
+function calcularPromedio(){
+    let suma = 0;
+    for (let i = 0; i < $numeros.length; i++){
+        suma = suma + Number($numeros[i].innerText);
     }
     
+    let promedio = suma / $numeros.length;
     
+    return promedio;
+}
+
+let arrayNumeros = [];
+
+for(let i = 0; i < $numeros.length; i++){
+    arrayNumeros.push(Number($numeros[i].innerText))
 }
 
 
-document.querySelector('#numero-mas-pequeño').textContent = obtenerNumeroMenor(arrayDeNumeros);
-document.querySelector('#numero-mas-grande').textContent = obtenerNumeroMayor(arrayDeNumeros);
-document.querySelector('#numeros-promedio').textContent = Math.floor(calcularPromedio(arrayDeNumeros));
-document.querySelector('#numero-mas-repetido').textContent = obtenerNumeroMasRepetido(arrayDeNumeros);
+
+
+
+function verificarNumeroMasRepetido(){
+
+    let contadorActual = 0
+    let contador = 0
+    let numeroFrecuente = 0;
+
+    for (i = 0; i < arrayNumeros.length; i++){
+        for ( j = i + 1; j < arrayNumeros.length; j++){
+                if (arrayNumeros[i] === arrayNumeros[j]){
+                    contadorActual++;
+                    if (contadorActual > contador) {
+                        
+                        numeroFrecuente = arrayNumeros[i];
+                        contador = contadorActual;
+                    }
+                }
+            }
+        contadorActual = 0;
+        } 
+    
+ return numeroFrecuente;
+}
+
+
+
+
+
+
+document.querySelector('#numero-mas-pequeño').textContent = `El numero menor es: ${obtenerNumeroMenor()}`;
+document.querySelector('#numero-mas-grande').textContent = `El numero mayor es: ${obtenerNumeroMayor()}`;
+document.querySelector('#numeros-promedio').textContent = `El promedio de los numeros es: ${calcularPromedio()}`;
+document.querySelector('#numero-mas-repetido').textContent =`El numero más repetido es: ${verificarNumeroMasRepetido()}`;
